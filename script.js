7bad1102a4773ffc7fc538e014fb30e55543bea3
ahmadramzan44 <pyarey323@gmail.com>
2026-07-27 15:04:23 +0100

    Replace hardcoded credentials with constants

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function login(){
     const user = document.getElementById("user").value;
     const pass = document.getElementById("pass").value;
 
-    if(user === "admin" && pass === "admin"){
+    if(user === USERNAME && pass === PASSWORD){
 
         document.getElementById("login").style.display = "none";
         document.getElementById("dashboard").style.display = "block";
