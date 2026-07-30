9af88d1bbf10b8651a6c4f8732de1e771fe8fbc3
ahmadramzan44 <pyarey323@gmail.com>
2026-07-30 11:02:22 +0100

    Enhance logging and modify buy signal conditions
    Added logging for trend values and adjusted buy conditions.

--- signals.js
+++ signals.js
@@ -420,7 +420,7 @@ console.log({
     sellVolume: volume >= 0.10,
     sellTrend: trend4h === "Bearish"
 });     
-     
+    console.log("trend1h =", trend1h, "trend4h =", trend4h); 
 // STRONG BUY
 if(
 
@@ -440,16 +440,14 @@ if(
 }
 
 // BUY
-else if(
-
-    bullish >= 4 &&
-    score >= 20 &&
-    macd > 0 &&
-    adx >= 15 &&
-    volume >= 0.10 &&
-    trend1h === "Bullish"
-
-){
+console.log({
+    bullish: bullish >= 5,
+    score: score >= 40,
+    macd: macd > 0,
+    adx: adx >= 20,
+    volume: volume >= 0.15,
+    trend1h: trend1h === "Bullish"
+});
 
     console.log("✅ BUY PASSED");
 
